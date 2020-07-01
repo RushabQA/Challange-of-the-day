1748a727be55e9fd23f7c6616c745850d9aa17b8
RushabQA <65768895+RushabQA@users.noreply.github.com>
2020-07-01 23:51:03 +0100

    Add files via upload

---
+++ 01.07.2020_Challange_Of_Day.py
@@ -0,0 +1,11 @@
+def list(L):
+    odd_squares = []
+    for i in list(L):
+        if i % 2 != 0:
+            odd_squares.append(i ** 2)
+
+    return odd_squares 
+
+
+L= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(odd_squares.append())
